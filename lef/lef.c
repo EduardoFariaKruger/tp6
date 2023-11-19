530420d34d963f2dbc3409b1b7d781d3ec760e24
EduardoFariaKruger <90162399+EduardoFariaKruger@users.noreply.github.com>
2023-11-19 18:56:37 -0300

    so vai

---
+++ lef/lef.c
@@ -0,0 +1,143 @@
+#include "lef.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* 
+ * Cria um evento contendo os parâmetros informados.
+ * Retorna um ponteiro para o evento ou NULL se falhar.
+ */
+struct evento_t *cria_evento (int tempo, int tipo, int dado1, int dado2)
+{
+    struct evento_t *evento;
+    if (!(evento = malloc(sizeof(struct lef_t))))
+    {
+        return NULL;
+    }
+
+    evento->tempo = tempo;
+    evento->tipo  = tipo;
+    evento->dado1 = dado1;
+    evento->dado2 = dado2;
+
+    return evento;
+}
+
+/* 
+ * Destroi um evento e retorna NULL.
+ */ 
+struct evento_t *destroi_evento (struct evento_t *e)
+{
+    free(e);
+    e = NULL;
+    return NULL;
+}
+
+/* 
+ * Cria uma LEF vazia 
+ * Retorna um ponteiro para a LEF ou NULL se falhar.
+ */
+struct lef_t *cria_lef ()
+{
+    struct lef_t *lef;
+    if (!(lef = malloc(sizeof(struct lef_t))))
+    {
+        return NULL;
+    }
+    lef->primeiro = NULL;
+
+    return lef;
+}
+
+/* 
+ * Destroi a LEF, liberando a memória ocupada por ela, inclusive os eventos.
+ * Retorna NULL.
+ */ 
+struct lef_t *destroi_lef (struct lef_t *l)
+{
+    struct nodo_lef_t *p = l->primeiro;
+    struct nodo_lef_t *temp;
+    while(p->prox != NULL)
+    {
+        temp = p;
+        destroi_evento(p->evento);
+        p = p->prox;
+        free(temp);
+    }
+    destroi_evento(l->primeiro);
+    free(l->primeiro);
+    return NULL;
+}
+
+/*
+ * Insere o evento apontado na LEF na ordem de tempos crescentes.
+ * Eventos com o mesmo tempo devem respeitar a politica FIFO.
+ * Retorna 1 em caso de sucesso ou 0 caso contrario.
+*/
+int insere_lef (struct lef_t *l, struct evento_t *e)
+{
+    if(l == NULL)
+    {
+        return 0;
+    }
+    
+    struct nodo_lef_t *novo;
+    if (!(novo = malloc(sizeof(struct lef_t))))
+    {
+        return 0;
+    }
+    novo->prox = (l->primeiro)->prox;
+    l->primeiro = novo;
+    novo->evento = e;
+    return 1;
+}
+
+/* 
+ * Retira o primeiro evento da LEF.
+ * Retorna ponteiro para o evento ou NULL se falhar.
+ */
+struct evento_t *retira_lef (struct lef_t *l)
+{
+    if(l == NULL || l->primeiro == NULL)
+    {
+        return NULL;
+    }
+    struct nodo_lef_t *temp;
+    temp = l->primeiro;
+    l->primeiro = temp->prox;
+    return temp;
+}
+
+/* 
+ * Informa se a LEF está vazia.
+ * Retorna 1 se vazia e 0 senao ou se falhar.
+ */
+int vazia_lef (struct lef_t *l)
+{
+    if(l == NULL)
+    {
+        return 0;
+    }
+    return (l->primeiro != NULL);
+}
+
+/* 
+ * Imprime a LEF. Não faz parte do TAD, mas serve para depuração.
+ * Formato de saída:
+ * tempo tipo dado1 dado2
+ * tempo tipo dado1 dado2
+ * ...
+ * total XX eventos
+ */
+void imprime_lef (struct lef_t *l)
+{
+    if(l == NULL)
+    {
+        return;
+    }
+    struct nodo_lef_t *p = l->primeiro;
+    while(p->prox != NULL)
+    {
+        printf("%d %d %d %d", p->evento->tempo, p->evento->tipo, p->evento->dado1, p->evento->dado2);
+    }
+}
